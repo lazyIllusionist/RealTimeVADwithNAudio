b5dd0c8641f2c2b60b4778d5a47c5c56b70831b0
lazyIllusionist <skytenkoroman@gmail.com>
2019-04-15 19:14:19 +0300

    First commit

---
+++ RealTimeVAD/RealTimeVAD/Detectors/ShortTimeEnergyDetector.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+
+namespace RealTimeVAD.Detectors
+{
+    class ShortTimeEnergyDetector : AbstractDetector
+    {
+        double barrier = 0.08; //Get by empiric
+        int framesWithVoise;
+
+        byte[][] _buffer;
+
+        public bool Decoding()
+        {
+            framesWithVoise = 0;
+            try
+            {
+                int counter = 0;
+                /*
+                 * Break 100ms frame to ten 10ms frames
+                 * Multiply by 2 because information about 1 sempl writes in 2 byte
+                 */
+                _buffer = fraim.Buffer.GroupBy(_ => counter++ / (fraimSampleRate / 100 * 2)).Select(elem => elem.ToArray()).ToArray();
+
+                foreach (var i in _buffer)
+                    if (Decoder(i, barrier))
+                        framesWithVoise++;
+
+                base.ind = true;
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+        }
+
+        bool Decoder(byte[] _buffer, double barrier)
+        {
+            double Sum2 = 0;
+            bool Tr = false;
+
+            for (int index = 0; index < _buffer.Length; index += 2)
+            {
+                double Tmp = (short)((_buffer[index + 1] << 8) | _buffer[index]);
+                Tmp /= 32768.0;
+                Sum2 += Tmp * Tmp;
+                if (Tmp > barrier)
+                    Tr = true; 
+            }
+            Sum2 /= _buffer.Length / 2;
+            if (Tr || Sum2 > barrier)
+                return true;
+            else
+                return false;
+        }
+
+        public new bool VoiceIsPresent
+        {
+            get
+            {
+                //if (!base.ind)
+                //{
+                    this.Decoding();
+                    this.AddResults(framesWithVoise);
+               // }
+                return result;
+            }
+        }
+    }
+}
---
+++ RealTimeVAD/RealTimeVAD/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio;
+using NAudio.Wave;
+
+namespace RealTimeVAD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool ind = false;
+        Detectors.ShortTimeEnergyDetector sted;
+        WaveIn waveIn;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Initialize();
+            StartButton.Content = "Press to start";
+            StartButton.Click += Click;
+        }
+
+        bool Initialize()
+        {
+            try
+            {
+                sted = new Detectors.ShortTimeEnergyDetector();
+                //{
+                //    fraimSampleRate = 8000,
+                //    percentOfSucsess = 30
+                //};
+
+                waveIn = new WaveIn();
+                waveIn.DeviceNumber = 0;
+                waveIn.DataAvailable += (object sender, WaveInEventArgs e) => 
+                {
+                    sted.fraim = e;
+                    if (sted.VoiceIsPresent)
+                    {
+                        StartButton.Background = Brushes.Green;
+                        StartButton.Foreground = Brushes.Black;
+                    }
+                    else
+                    {
+                        StartButton.Background = Brushes.Red;
+                        StartButton.Foreground = Brushes.White;
+                    }
+                };
+                waveIn.RecordingStopped += waveIn_RecordingStopped;
+                waveIn.WaveFormat = new WaveFormat(8000, 1);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        void Click(object obj, RoutedEventArgs e)
+        {
+            if (sted == null || waveIn == null)
+                Initialize();
+            try
+            {
+                if (ind)
+                {
+                    waveIn.StopRecording();
+                    StartButton.Background = Brushes.White;
+                    StartButton.Foreground = Brushes.Black;
+                    StartButton.Content = "Press to start";
+                    ind = false;
+                }
+                else
+                {
+                    waveIn.StartRecording();
+                    StartButton.Content = "If this button:\ngreen something is speaking\nred - no voice outthere";
+                    ind = true;
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+
+        private void waveIn_RecordingStopped(object sender, EventArgs e)
+        {
+                waveIn.Dispose();
+                waveIn = null;
+        }
+    }
+
+}
